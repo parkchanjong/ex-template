d6b4ae84d79cbc728a1593f7a978648e99c27b33
parkchanjong <7000cj@gmail.com>
2019-04-12 22:13:11 +0900

    static

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
 	res.send('Hello World!');
+});
+
+app.get('/login', function(req, res) {
+	res.send('<h1>login plz</h1>');
 });
 
 app.listen(3000, function() {
