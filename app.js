dc78607645b707ef1374559497ab7d3ad166a544
parkchanjong <7000cj@gmail.com>
2019-04-14 20:21:54 +0900

    jade

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+app.locals.pretty = true;
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.render('index', { title: 'Hey', message: 'Hello there!', time: Date() });
+});
 
 app.get('/', function(req, res) {
 	res.send('Hello World!');
